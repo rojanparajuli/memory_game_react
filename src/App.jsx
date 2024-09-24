e878df2ca627e0c14415749856442cd41256e45f
Rojan Parajuli <rojanparajuli13@gmail.com>
2024-09-24 13:26:45 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import './App.css';
 import './css/home_css.css';
 import MemoryGame from './screen/HomeScreen';
 
